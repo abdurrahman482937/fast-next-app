23ad769b14a92be3a73ce1dd1a4da4097a20fec1
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-03-07 21:24:45 +0600

    commit 5

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,5 +1,20 @@
-export default function post({params}) {
+import getPost from "@/app/libs/getPost";
+
+export async function generateMetadata({ params }) {
+  const post = await getPost(params.id);
+  return {
+    title: `${post.title} - my next app`,
+    description: post.body,
+  };
+}
+
+export default async function post({ params }) {
+  const post = await getPost(params.id);
   return (
-    <div>{params.id}</div>
-  )
+    <div className="mt-4 h-auto w-auto py-4 px-3">
+      <text>{post.title}</text>
+      <br />
+      <text>{post.body}</text>
+    </div>
+  );
 }
